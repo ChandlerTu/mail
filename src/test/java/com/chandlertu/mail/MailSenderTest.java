b876e5eaf8c96faf9db977cdbd948bc2b62ddb0a
Chandler Tu <chandlertu@chandlertu.com>
2017-07-05 17:03:09 +0800

    Added Properties

--- src/test/java/com/chandlertu/mail/MailSenderTest.java
+++ src/test/java/com/chandlertu/mail/MailSenderTest.java
@@ -2,6 +2,7 @@ package com.chandlertu.mail;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Properties;
@@ -14,7 +15,7 @@ public class MailSenderTest {
 	public void testSend() {
 		Properties props = new Properties();
 		Path path = Paths.get(System.getProperty("user.home"), ".chandlertu", "mail.properties");
-		try (FileInputStream inStream = new FileInputStream(path.toFile())) {
+		try (InputStreamReader inStream = new InputStreamReader(new FileInputStream(path.toFile()), "utf-8")) {
 			props.load(inStream);
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -23,9 +24,9 @@ public class MailSenderTest {
 		String account = props.getProperty("account");
 		String password = props.getProperty("password");
 		String smtp = props.getProperty("smtp");
-		String to = account;
-		String subject = "test";
-		String text = "";
+		String to = props.getProperty("to");
+		String subject = props.getProperty("subject");
+		String text = props.getProperty("text");
 		MailSender sender = new MailSender(account, password, smtp);
 		sender.send(to, subject, text);
 	}
